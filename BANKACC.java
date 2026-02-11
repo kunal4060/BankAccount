90e49989c9d7b140629c7c957cd671875a8c50fd
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-11 18:53:40 +0530

    initialize the constructer

---
+++ BANKACC.java
@@ -0,0 +1,19 @@
+public class BANKACC {
+    String name;
+    String pancard;
+    String aadharcard;
+    double balance;
+    
+       
+    // for making a new acc
+   
+    BANKACC(String name, String pancard, String aadharcard, double balance) {
+        this.name = name;
+        this.pancard = pancard;
+        this.aadharcard = aadharcard;
+        this.balance = balance;
+
+    }
+    
+    
+}
