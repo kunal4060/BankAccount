1a32af3d81c5c6a76668613f7fe0116449ebffc0
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-11 19:15:09 +0530

    maiya chod di bhai mene code aur logic ki

--- BANKSYS.java
+++ BANKSYS.java
@@ -1,4 +1,13 @@
 public class BANKSYS {
+    BANKACC[] accounts = new BANKACC[100]; 
+    //hamare yaha itne hi accounts ban sakte hai
+    int count = 0; 
+    void createaccount(String name, String pancard, String aadharcard, double balance){
+       accounts[count] = new BANKACC(name, pancard, aadharcard, balance);
+       count++;
+       System.out.println("aapke acc ab hamare aur aapke ke bich me aaya hai ji");
+    }
+
    
     
 }
